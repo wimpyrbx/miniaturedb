148c7c79a04feefc400703b3eb5883900fa3dbfd
Wimpy <wimpy.rbx@gmail.com>
2024-12-01 14:27:21 +0100

    update

--- src/components/themes/themeselect/index.ts
+++ src/components/themes/themeselect/index.ts
@@ -1,7 +1,7 @@
 import { Theme } from "../../../lib/theme";
 import { forest } from "./forest";
 import { ocean } from "./ocean";
-import { sunset } from "../../theme/sunset";
+import { sunset } from "./sunset";
 import { midnight } from "./midnight";
 
 export const themes: Theme[] = [
